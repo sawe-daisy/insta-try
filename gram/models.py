616ad6224b5808a2f7f0348eb8d2309c2cff0ace
sawe-daisy <deesawe16@gmail.com>
2020-11-20 13:48:55 +0300

    first model migrations

--- gram/models.py
+++ gram/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Image(models.Model):
     image= CloudinaryField('image')
-    
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.CharField(max_length=30)
+    caption= models.TextField()
+    pub_date=models.DateTimeField(auto_now_add=True)
+
+
+
