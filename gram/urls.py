026ef7837d2ac18a8f2016a94047f1431b2e4fb5
sawe-daisy <deesawe16@gmail.com>
2020-11-20 18:10:32 +0300

    adding registration form

--- gram/urls.py
+++ gram/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 
 urlpatterns = [
-    path('', views.index, name='landing'),
+    path('', views.index, name='gram-landing'),
+    path('register/',views.register, name='registration'),
 ]
--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image
 from django.http import HttpRequest
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,5 +11,12 @@ def index(request):
     return render(request, 'index.html', {"post":post})
 
 def register(request):
-    form= UserCreationForm()
+    if request.method=="POST":
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get('username')
+            messages.success(request, f'Successfully created Account for {username}!')
+            return redirect('gram-landing')
+    else:
+        form= UserCreationForm()
     return render(request, 'users/register.html', {"form":form})
