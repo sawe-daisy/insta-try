d3ab389de35130b2b3e223b94c5f02b5b8f9758d
sawe-daisy <deesawe16@gmail.com>
2020-11-20 18:37:28 +0300

    profile creation form

--- gram/forms.py
+++ gram/forms.py
@@ -5,3 +5,9 @@ class RegistrationForm(forms.Form):
     username=forms.CharField(label= 'Username', max_length=20)
     bio= forms.CharField(label='Tell us about yourself', max_length=50)
     prof_pic=forms.ImageField(label='AVI')
+
+class ImageUploadForm(forms.Form):
+    image = forms.ImageField(label = "Image:")
+    name = forms.CharField(label = "Image Name:", max_length=50)
+    caption = forms.CharField(label = "Image Caption:", max_length=300)
+    
