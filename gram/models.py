328f73ccbb544b25a3ec46664589d5310b773078
sawe-daisy <deesawe16@gmail.com>
2020-11-20 15:00:59 +0300

    landing page template

--- gram/models.py
+++ gram/models.py
@@ -11,5 +11,5 @@ class Image(models.Model):
     caption= models.TextField()
     pub_date=models.DateTimeField(auto_now_add=True)
 
-
-
+    def __str__(self):
+        return self.name
--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Image
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def index(request):
+    post= Image.objects.all()
+
+    return render(request, 'index.html', {"post":post})
+
+def register(request):
+    form= UserCreationForm()
+    return render(request, 'users/register.html', {"form":form})
