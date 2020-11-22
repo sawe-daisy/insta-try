82d91b7d139d1e9a132a97af688053eb318df4e4
sawe-daisy <deesawe16@gmail.com>
2020-11-22 10:32:38 +0300

    connecting profile to user

--- gram/models.py
+++ gram/models.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user=models.OneToOneField(User, blank=True, on_delete=models.CASCADE)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     bio= models.CharField(max_length=100)
-    email= models.EmailField()
     prof_pic= CloudinaryField('image', blank=True)
 
     def __str__(self):
--- gram/urls.py
+++ gram/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='gram-landing'),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('register/',views.register, name='registration'),
     path('profile/',views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
